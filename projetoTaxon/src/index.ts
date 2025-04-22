23c9e01dbbb6fe0e760d605b395ac32dae007a24
lucashajime <142362263+lucashajime@users.noreply.github.com>
2025-04-22 19:40:03 -0300

    Update index.ts

--- projetoTaxon/src/index.ts
+++ projetoTaxon/src/index.ts
@@ -2,4 +2,3 @@ import MainScreen from "./view/MainScreen";
 import TaxonomyController from "./controller/TaxonomyController";
 
 const controller = new TaxonomyController();
-new MainScreen(controller);
