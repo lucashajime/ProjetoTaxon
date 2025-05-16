ed8f9aae9ce91e7216d628529695c03fee241e84
lucashajime <142362263+lucashajime@users.noreply.github.com>
2025-05-16 14:34:43 -0300

    Add files via upload

---
+++ projetoTaxon/jest.config.js
@@ -0,0 +1,15 @@
+const { createDefaultPreset } = require("ts-jest/presets")
+
+const tsJestTransformCfg = createDefaultPreset().transform
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>'],
+};
